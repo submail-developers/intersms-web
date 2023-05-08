70249f0cfdabdef37e160087750cf4d26d5a7b67
Shijuntao-ux <18438621787@163.com>
2023-05-08 14:15:37 +0800

    解决热更新报错问题2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { FC } from 'react';
 import { RouterProvider } from "react-router-dom";
 import { Provider } from 'react-redux';
 import router from '@/routes';
 import { store } from '@/store';
 import Loading from '@/components/loading/loading';
 
-const App: FC = () => {
+export default function App(){
   return (
     <Provider store={store}>
       <RouterProvider router={router} />
@@ -13,5 +12,3 @@ const App: FC = () => {
     </Provider>
   )
 }
-
-export default App
