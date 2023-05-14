3c04065d37ac9baf7497ec957d446bff15414432
Shijuntao-ux <18438621787@163.com>
2023-05-14 18:32:39 +0800

    网络信息配置

--- src/pages/console/network/network.tsx
+++ src/pages/console/network/network.tsx
@@ -1,6 +1,275 @@
+import { useEffect, useState, MutableRefObject, useRef } from 'react'
+import {
+  Button,
+  Select,
+  Form,
+  Input,
+  DatePicker,
+  ConfigProvider,
+  Table,
+  App,
+  Row,
+  Col,
+  Checkbox,
+} from 'antd'
+import type { ColumnsType } from 'antd/es/table'
+import AddDialog from './dialog/addDialog'
+import MenuTitle from '@/components/menuTitle/menuTitle'
+import type { Dayjs } from 'dayjs'
+import { useSize } from '@/hooks'
+import { API } from 'apis'
+import type { CheckboxChangeEvent } from 'antd/es/checkbox'
+
 import './network.scss'
 
-// 网络信息配置
-export default function Fn() {
-  return <div data-class='network'>network</div>
+interface DataType {
+  id: string
+  net_type: string
+  country_name: string
+  country_code: string
+  area_name: string
+  price1: string
+  price2: string
+}
+interface FormValues {
+  channel: string
+  group: string
+  time: [Dayjs, Dayjs] | null
+  keyword: string
+}
+
+// 国家信息配置
+export default function Channel() {
+  const addDialogRef: MutableRefObject<any> = useRef(null)
+  const { Option } = Select
+  const { RangePicker } = DatePicker
+  const size = useSize()
+  const [form] = Form.useForm()
+  const { message } = App.useApp()
+
+  const channelList = [
+    { label: '全部通道', value: 'all' },
+    { label: '通道1', value: '1' },
+    { label: '通道2', value: '2' },
+  ]
+  const groupList = [
+    { label: '全部通道组', value: 'all' },
+    { label: '通道组1', value: '1' },
+    { label: '通道组2', value: '2' },
+  ]
+
+  const onFinish = (values: FormValues) => {}
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: <Checkbox></Checkbox>,
+      dataIndex: 'checkbox',
+      className: 'checkbox-wrap',
+      width: 80,
+      render: (_, record) => (
+        <Checkbox
+          className='checkbox'
+          onChange={(e) => onChange(e, record)}></Checkbox>
+      ),
+    },
+    {
+      title: '网络类型',
+      dataIndex: 'new_type',
+      width: 180,
+      ellipsis: true,
+      render: (_, record) => (
+        <span className='color'>{record.country_name}</span>
+      ),
+      onCell: (record: DataType) => {
+        return {
+          onClick: () => {
+            setSelectedRowKeys([record.id])
+          },
+        }
+      },
+    },
+    {
+      title: '国家名称',
+      dataIndex: 'country_name',
+      width: 180,
+      ellipsis: true,
+      onCell: (record: DataType) => {
+        return {
+          onClick: () => {
+            setSelectedRowKeys([record.id])
+          },
+        }
+      },
+    },
+    {
+      title: '国家代码',
+      dataIndex: 'country_code',
+      onCell: (record: DataType) => {
+        return {
+          onClick: () => {
+            setSelectedRowKeys([record.id])
+          },
+        }
+      },
+    },
+    {
+      title: '地区名称',
+      dataIndex: 'area_name',
+      onCell: (record: DataType) => {
+        return {
+          onClick: () => {
+            setSelectedRowKeys([record.id])
+          },
+        }
+      },
+    },
+    {
+      title: '成本价格',
+      dataIndex: 'price1',
+      onCell: (record: DataType) => {
+        return {
+          onClick: () => {
+            setSelectedRowKeys([record.id])
+          },
+        }
+      },
+    },
+    {
+      title: '建议零售价格',
+      dataIndex: 'price2',
+      onCell: (record: DataType) => {
+        return {
+          onClick: () => {
+            setSelectedRowKeys([record.id])
+          },
+        }
+      },
+    },
+  ]
+
+  // 被点击的客户(不是被checkbox选中的客户)
+  const [selectedRowKeys, setSelectedRowKeys] = useState<number[] | string[]>(
+    [],
+  )
+  // checkbox勾选的客户
+  const [checkedIds, setcheckedIds] = useState<string[]>([])
+
+  // checkbox勾选的事件
+  const onChange = (e: CheckboxChangeEvent, record: DataType) => {
+    if (e.target.checked) {
+      setcheckedIds([...checkedIds, record.id])
+    } else {
+      setcheckedIds(checkedIds.filter((account) => account !== record.id))
+    }
+  }
+
+  const rowSelection = {
+    selectedRowKeys,
+    hideSelectAll: true,
+    columnWidth: 4,
+    renderCell: () => {
+      return null
+    },
+  }
+
+  const data: DataType[] = []
+  for (let i = 0; i < 100; i++) {
+    data.push({
+      id: 'id' + i,
+      net_type: 'Chain',
+      country_name: '中国' + i,
+      country_code: 'CN',
+      area_name: '+86',
+      price1: 'string',
+      price2: 'string',
+    })
+  }
+
+  const updateCountryEvent = () => {
+    addDialogRef.current.open()
+  }
+
+  return (
+    <div data-class='network'>
+      <MenuTitle title='网络信息配置'></MenuTitle>
+      <Row justify='space-between' wrap align={'bottom'}>
+        <Col>
+          <div className='btn-group' style={{ marginBottom: '10px' }}>
+            <div className='btn' onClick={updateCountryEvent}>
+              <i className='icon iconfont icon-bianji'></i>
+              <span>新增</span>
+            </div>
+            <div className='btn' onClick={updateCountryEvent}>
+              <i className='icon iconfont icon-bianji'></i>
+              <span>编辑</span>
+            </div>
+            <div className='btn delete'>
+              <i className='icon iconfont icon-bianji'></i>
+              <span>删除</span>
+            </div>
+          </div>
+        </Col>
+        <Col>
+          <ConfigProvider
+            theme={{
+              token: {
+                controlHeight: 40,
+              },
+            }}>
+            <Form
+              name='basic'
+              form={form}
+              initialValues={{ channels_type: 'all' }}
+              layout='inline'
+              wrapperCol={{ span: 24 }}
+              onFinish={onFinish}
+              autoComplete='off'>
+              <Form.Item label='' name='country' style={{ marginBottom: 10 }}>
+                <Input
+                  size={size}
+                  placeholder='网络名称/国家/国家代码'
+                  maxLength={20}
+                  style={{ width: 220 }}></Input>
+              </Form.Item>
+              <Form.Item style={{ marginBottom: 10 }}>
+                <ConfigProvider
+                  theme={{
+                    token: {
+                      colorPrimary: '#ff5e2d',
+                      colorPrimaryHover: '#ff5e2d',
+                    },
+                  }}>
+                  <Button
+                    type='primary'
+                    size={size}
+                    htmlType='submit'
+                    style={{ width: 110, marginLeft: 0 }}>
+                    搜索
+                  </Button>
+                </ConfigProvider>
+              </Form.Item>
+            </Form>
+          </ConfigProvider>
+        </Col>
+      </Row>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorBgContainer: 'transparent',
+          },
+        }}>
+        <Table
+          className='theme-cell bg-white reset-table'
+          columns={columns}
+          dataSource={data}
+          rowSelection={rowSelection}
+          rowKey={'id'}
+          sticky
+          pagination={false}
+          scroll={{ x: 'max-content' }}
+        />
+      </ConfigProvider>
+      <AddDialog ref={addDialogRef} />
+    </div>
+  )
 }
