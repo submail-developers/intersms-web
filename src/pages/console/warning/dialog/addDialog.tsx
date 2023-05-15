bd4667e0bbb0bf19b724c9e9132eb4db2400a620
Shijuntao-ux <18438621787@163.com>
2023-05-15 10:46:26 +0800

    报警设置

---
+++ src/pages/console/warning/dialog/addDialog.tsx
@@ -0,0 +1,174 @@
+import { useState, useImperativeHandle, forwardRef } from 'react'
+import { Modal, Form, Input, App, Row, Col, Select, Radio } from 'antd'
+import { addAccount } from '@/api'
+import ModelFooter from '@/components/antd/modelFooter/modelFooter'
+import type { RadioChangeEvent } from 'antd'
+import './addDialog.scss'
+
+interface Props {
+  // onSearch: () => void
+}
+
+const Dialog = (props: Props, ref: any) => {
+  const [form] = Form.useForm()
+  const { message } = App.useApp()
+  useImperativeHandle(ref, () => {
+    return {
+      open,
+    }
+  })
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const open = () => {
+    form.resetFields()
+    setIsModalOpen(true)
+  }
+
+  const handleOk = async () => {
+    try {
+      const params = await form.validateFields()
+      const res = await addAccount(params)
+      if (res) {
+        message.success('保存成功！')
+      }
+      setIsModalOpen(false)
+    } catch (error) {}
+  }
+
+  const handleCancel = () => {
+    setIsModalOpen(false)
+  }
+
+  const onFinish = () => {}
+  const onFinishFailed = () => {}
+
+  const onChange = (e: RadioChangeEvent) => {
+    console.log('checked = ', e)
+  }
+
+  const onChange1 = (value: string) => {
+    console.log(`selected ${value}`)
+  }
+
+  const onSearch = (value: string) => {
+    console.log('search:', value)
+  }
+
+  return (
+    <Modal
+      title='报警设置'
+      width={640}
+      closable={false}
+      wrapClassName='modal-reset'
+      data-class='net-config-dialog'
+      footer={<ModelFooter onOk={handleOk} onCancel={handleCancel} />}
+      open={isModalOpen}>
+      <Form
+        name='form'
+        form={form}
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 24 }}
+        layout='vertical'
+        initialValues={{ type: '1', status: '1' }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete='off'>
+        <Row justify='space-between' gutter={30}>
+          <Col span={12}>
+            <Form.Item label='报警类型' name='type'>
+              <Select
+                showSearch
+                // bordered={false}
+                placeholder='请选择'
+                optionFilterProp='children'
+                onChange={onChange1}
+                onSearch={onSearch}
+                filterOption={(input, option) =>
+                  (option?.label ?? '')
+                    .toLowerCase()
+                    .includes(input.toLowerCase())
+                }
+                options={[
+                  {
+                    value: '1',
+                    label: '国家报警',
+                  },
+                  {
+                    value: '2',
+                    label: '通道报警',
+                  },
+                ]}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label='选择国家'>
+              <Select
+                showSearch
+                // bordered={false}
+                placeholder='请选择'
+                optionFilterProp='children'
+                onChange={onChange1}
+                onSearch={onSearch}
+                filterOption={(input, option) =>
+                  (option?.label ?? '')
+                    .toLowerCase()
+                    .includes(input.toLowerCase())
+                }
+                options={[
+                  {
+                    value: '1',
+                    label: '国家1',
+                  },
+                  {
+                    value: '2',
+                    label: '通道2',
+                  },
+                ]}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <div className='group-title'>报警条件</div>
+          </Col>
+        </Row>
+        <Row justify='space-between' gutter={30}>
+          <Col span={12}>
+            <Form.Item
+              label='报警时间范围'
+              name='time'
+              validateTrigger='onSubmit'>
+              <Input placeholder='请输入报警时间范围' />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label='报警最小条数' name='min_length'>
+              <Input placeholder='请输入最小条数' />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row justify='space-between' gutter={30}>
+          <Col span={12}>
+            <Form.Item label='失败率' name='fail' validateTrigger='onSubmit'>
+              <Input placeholder='请输入失败率' />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label='报警开关' name='status'>
+              <Radio.Group
+                options={[
+                  { label: '打开', value: '1' },
+                  { label: '关闭', value: '2' },
+                ]}
+                onChange={onChange}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </Modal>
+  )
+}
+export default forwardRef(Dialog)
--- src/pages/console/warning/warning.tsx
+++ src/pages/console/warning/warning.tsx
@@ -1,6 +1,315 @@
+import { useEffect, useState, MutableRefObject, useRef } from 'react'
+import {
+  Button,
+  Select,
+  Form,
+  Input,
+  DatePicker,
+  ConfigProvider,
+  Table,
+  App,
+  Row,
+  Col,
+  Checkbox,
+  Switch,
+} from 'antd'
+import type { ColumnsType } from 'antd/es/table'
+import AddDialog from './dialog/addDialog'
+import MenuTitle from '@/components/menuTitle/menuTitle'
+import type { Dayjs } from 'dayjs'
+import { useSize } from '@/hooks'
+import { API } from 'apis'
+import type { CheckboxChangeEvent } from 'antd/es/checkbox'
 import './warning.scss'
 
-// 报警设置
-export default function Fn() {
-  return <div data-class='warning'>warning</div>
+interface DataType {
+  id: string
+  warning_type: string
+  name: string
+  times: string
+  min_length: string
+  fail: string
+  status: string
+}
+interface FormValues {
+  channel: string
+  group: string
+  time: [Dayjs, Dayjs] | null
+  keyword: string
+}
+
+// 国家信息配置
+export default function NumberChannelsRoute() {
+  const addDialogRef: MutableRefObject<any> = useRef(null)
+  const { Option } = Select
+  const { RangePicker } = DatePicker
+  const size = useSize()
+  const [form] = Form.useForm()
+  const { message } = App.useApp()
+
+  const typeList = [
+    { label: '报警类型', value: 'all' },
+    { label: '报警类型1', value: '1' },
+    { label: '报警类型2', value: '2' },
+  ]
+  const groupList = [
+    { label: '全部通道组', value: 'all' },
+    { label: '通道组1', value: '1' },
+    { label: '通道组2', value: '2' },
+  ]
+
+  const onFinish = (values: FormValues) => {}
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: <Checkbox></Checkbox>,
+      className: 'checkbox-wrap',
+      width: 80,
+      render: (_, record) => (
+        <Checkbox
+          className='checkbox'
+          onChange={(e) => onChange(e, record)}></Checkbox>
+      ),
+    },
+    {
+      title: '报警类型',
+      dataIndex: 'warning_type',
+      width: '20%',
+      onCell: (record: DataType) => {
+        return {
+          onClick: () => {
+            setSelectedRowKeys([record.id])
+          },
+        }
+      },
+    },
+    {
+      title: '名称',
+      dataIndex: 'name',
+      onCell: (record: DataType) => {
+        return {
+          onClick: () => {
+            setSelectedRowKeys([record.id])
+          },
+        }
+      },
+    },
+    {
+      title: '报警时间范围',
+      dataIndex: 'times',
+      onCell: (record: DataType) => {
+        return {
+          onClick: () => {
+            setSelectedRowKeys([record.id])
+          },
+        }
+      },
+    },
+    {
+      title: '报警最小条数',
+      dataIndex: 'min_length',
+      onCell: (record: DataType) => {
+        return {
+          onClick: () => {
+            setSelectedRowKeys([record.id])
+          },
+        }
+      },
+    },
+    {
+      title: '报警失败率',
+      dataIndex: 'fail',
+      onCell: (record: DataType) => {
+        return {
+          onClick: () => {
+            setSelectedRowKeys([record.id])
+          },
+        }
+      },
+    },
+    {
+      title: '报警开关',
+      render: (_, record) => {
+        return (
+          <>
+            <Switch size='small' checked={record.status == '1'} />
+            <span>{record.status == '1' ? '已启用' : '未启用'}</span>
+          </>
+        )
+      },
+      onCell: (record: DataType) => {
+        return {
+          onClick: () => {
+            setSelectedRowKeys([record.id])
+          },
+        }
+      },
+    },
+    // {
+    //   title: '操作',
+    //   render: (_, record) => (
+    //     <div>
+    //       <Button type='link' style={{ paddingLeft: 0, paddingRight: 0 }}>
+    //         编辑
+    //       </Button>
+    //       <Button type='link'>删除</Button>
+    //     </div>
+    //   ),
+    // },
+  ]
+
+  // 被点击的客户(不是被checkbox选中的客户)
+  const [selectedRowKeys, setSelectedRowKeys] = useState<number[] | string[]>(
+    [],
+  )
+  // checkbox勾选的客户
+  const [checkedIds, setcheckedIds] = useState<string[]>([])
+
+  // checkbox勾选的事件
+  const onChange = (e: CheckboxChangeEvent, record: DataType) => {
+    if (e.target.checked) {
+      setcheckedIds([...checkedIds, record.id])
+    } else {
+      setcheckedIds(checkedIds.filter((account) => account !== record.id))
+    }
+  }
+
+  const rowSelection = {
+    selectedRowKeys,
+    hideSelectAll: true,
+    columnWidth: 4,
+    renderCell: () => {
+      return null
+    },
+  }
+
+  const data: DataType[] = []
+  for (let i = 0; i < 100; i++) {
+    data.push({
+      id: 'id' + i,
+      warning_type: '国家报警',
+      name: '中国' + i,
+      times: '10分钟',
+      min_length: '1->100',
+      fail: '2%',
+      status: '' + (i % 2),
+    })
+  }
+
+  const updateCountryEvent = () => {
+    addDialogRef.current.open()
+  }
+
+  return (
+    <div data-class='numberChannelsRoute'>
+      <MenuTitle title='报警设置'></MenuTitle>
+      <Row justify='space-between' wrap align={'bottom'}>
+        <Col>
+          <div className='btn-group' style={{ marginBottom: '10px' }}>
+            <div className='btn' onClick={updateCountryEvent}>
+              <i className='icon iconfont icon-bianji'></i>
+              <span>新增</span>
+            </div>
+            <div className='btn' onClick={updateCountryEvent}>
+              <i className='icon iconfont icon-bianji'></i>
+              <span>编辑</span>
+            </div>
+            <div className='btn' onClick={updateCountryEvent}>
+              <i className='icon iconfont icon-tingyong'></i>
+              <span>停用</span>
+            </div>
+            <div className='btn delete'>
+              <i className='icon iconfont icon-bianji'></i>
+              <span>删除</span>
+            </div>
+          </div>
+        </Col>
+        <Col>
+          <ConfigProvider
+            theme={{
+              token: {
+                controlHeight: 40,
+              },
+            }}>
+            <Form
+              name='basic'
+              form={form}
+              initialValues={{ message_type: 'all', channels_type: 'all' }}
+              layout='inline'
+              wrapperCol={{ span: 24 }}
+              onFinish={onFinish}
+              autoComplete='off'>
+              <Form.Item label='' name='country' style={{ marginBottom: 10 }}>
+                <Input
+                  size={size}
+                  placeholder='国家/通道/账号'
+                  maxLength={20}
+                  style={{ width: 220 }}></Input>
+              </Form.Item>
+              <Form.Item
+                label=''
+                name='message_type'
+                style={{ marginBottom: 10 }}>
+                <Select
+                  placeholder='报警类型'
+                  style={{ width: 162 }}
+                  size={size}
+                  suffixIcon={
+                    <i
+                      className='icon iconfont icon-xiala'
+                      style={{
+                        color: '#000',
+                        fontSize: '12px',
+                        transform: 'scale(.45)',
+                      }}
+                    />
+                  }>
+                  {typeList.map((item) => (
+                    <Option value={item.value} key={item.value}>
+                      {item.label}
+                    </Option>
+                  ))}
+                </Select>
+              </Form.Item>
+              <Form.Item style={{ marginBottom: 10 }}>
+                <ConfigProvider
+                  theme={{
+                    token: {
+                      colorPrimary: '#ff5e2d',
+                      colorPrimaryHover: '#ff5e2d',
+                    },
+                  }}>
+                  <Button
+                    type='primary'
+                    size={size}
+                    htmlType='submit'
+                    style={{ width: 110, marginLeft: 0 }}>
+                    搜索
+                  </Button>
+                </ConfigProvider>
+              </Form.Item>
+            </Form>
+          </ConfigProvider>
+        </Col>
+      </Row>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorBgContainer: 'transparent',
+          },
+        }}>
+        <Table
+          className='theme-cell bg-white reset-table'
+          columns={columns}
+          dataSource={data}
+          rowSelection={rowSelection}
+          rowKey={'id'}
+          sticky
+          pagination={false}
+          scroll={{ x: 'max-content' }}
+        />
+      </ConfigProvider>
+      <AddDialog ref={addDialogRef} />
+    </div>
+  )
 }
