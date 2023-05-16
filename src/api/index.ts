5121b500658c3ec163d761b267014db47b0ab523
Shijuntao-ux <18438621787@163.com>
2023-05-16 17:24:42 +0800

    接口管理

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,10 @@
 import request from './request'
 import { API } from 'apis'
 
+/**
+ * 示例接口及测试接口start
+ */
+
 // 示例接口-get
 export const getPetInfo = (params: API.PetInfoParams) => {
   return request.get<any, API.Response<API.PetInfoRes>, API.PetInfoParams>(
@@ -27,6 +31,14 @@ export const getNumberList = () => {
   )
 }
 
+/**
+ * 示例接口及测试接口end
+ */
+
+/**
+ * 发送列表start
+ */
+
 // 获取发送列表
 export const getSendList = (data: API.GetSendListParams) => {
   return request.post<
@@ -35,6 +47,13 @@ export const getSendList = (data: API.GetSendListParams) => {
     API.GetSendListParams
   >('customer/get_send_list', { ...data })
 }
+/**
+ * 发送列表end
+ */
+
+/**
+ * 客户信息start
+ */
 
 // 获取客户列表
 export const getAccountList = (data: API.AccountListParams) => {
@@ -60,6 +79,78 @@ export const addAccount = (data: API.AddAccountParams) => {
     { ...data },
   )
 }
+
+/**
+ * 客户信息end
+ */
+
+/**
+ * 通道组管理start
+ */
+
+/**
+ * 通道组管理end
+ */
+
+/**
+ * 通道管理start
+ */
+
+/**
+ * 通道管理end
+ */
+
+/**
+ * 通道管理start
+ */
+
+/**
+ * 通道管理end
+ */
+
+/**
+ * 国家信息配置start
+ */
+
+/**
+ * 国家信息配置end
+ */
+
+/**
+ * 网络信息配置start
+ */
+
+/**
+ * 网络信息配置end
+ */
+
+/**
+ * 号码通道路由start
+ */
+
+/**
+ * 号码通道路由end
+ */
+
+/**
+ * 报警设置start
+ */
+
+/**
+ * 报警设置end
+ */
+
+/**
+ * 号码归属查询start
+ */
+
+/**
+ * 号码归属查询end
+ */
+
+/**
+ * 敏感词管理start
+ */
 
 // 获取敏感词列表
 export const GetSensitiveWordList = (data: API.GetSensitiveWordListParams) => {
@@ -97,3 +188,38 @@ export const SensitiveWordListStopUsing = (
     API.SensitiveWordListStopUsingParams
   >('customer/update_sensitive_keywords_status', { ...data })
 }
+/**
+ * 敏感词管理end
+ */
+
+/**
+ * 关键词管理start
+ */
+
+/**
+ * 关键词管理end
+ */
+
+/**
+ * 状态码对照表start
+ */
+
+/**
+ * 状态码对照表end
+ */
+
+/**
+ * 黑名单管理start
+ */
+
+/**
+ * 黑名单管理end
+ */
+
+/**
+ * 登陆start
+ */
+
+/**
+ * 登陆end
+ */
