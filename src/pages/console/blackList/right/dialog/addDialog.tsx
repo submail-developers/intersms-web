d3cacb71755d5076fe188e844f2acb283f480205
Shijuntao-ux <18438621787@163.com>
2023-06-30 14:58:38 +0800

    上传文件

--- src/pages/console/blackList/right/dialog/addDialog.tsx
+++ src/pages/console/blackList/right/dialog/addDialog.tsx
@@ -1,21 +1,9 @@
-import axios from 'axios'
-import { useState, useImperativeHandle, forwardRef } from 'react'
-import {
-  Modal,
-  Form,
-  Input,
-  App,
-  Row,
-  Col,
-  Radio,
-  Select,
-  Button,
-  Upload,
-} from 'antd'
+import { useState, useImperativeHandle, forwardRef, useRef } from 'react'
+import { Modal, Form, Input, App, Upload, Button } from 'antd'
 
 import { useAppSelector } from '@/store/hook'
 import { blackState } from '@/store/reducers/black'
-import { addBlackMobileList, uploadBlackMobileList } from '@/api'
+import { uploadBlackMobileList } from '@/api'
 import ModelFooter from '@/components/antd/modelFooter/modelFooter'
 import type { UploadFile, UploadProps } from 'antd/es/upload/interface'
 import { UploadOutlined } from '@ant-design/icons'
@@ -41,56 +29,89 @@ const Dialog = ({ onSearch }: Props, ref: any) => {
   }
 
   const [fileList, setFileList] = useState<UploadFile[]>([])
+  const copyFileList = useRef(null)
   const props: UploadProps = {
+    accept: '.txt, .xlsx, .xls, .csv, .vcf',
     onRemove: (file) => {
       const arr = fileList.filter((item) => item != file)
       setFileList(arr)
+      copyFileList.current = arr
     },
     beforeUpload: (file) => {
-      const isConform =
-        file.type === 'text/plain' ||
-        file.type === 'text/csv' ||
-        file.type === 'text/directory' ||
-        file.type ===
-          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ||
-        file.type === 'application/vnd.ms-excel'
-      if (!isConform) {
-        message.error(`${file.name} 不符合上传文件的格式`)
-        return isConform || Upload.LIST_IGNORE
-      }
-      setFileList([...fileList, file])
-      return false
+      // const isConform =
+      //   file.type === 'text/plain' ||
+      //   file.type === 'text/csv' ||
+      //   file.type === 'text/vcard' ||
+      //   file.type === 'text/directory' ||
+      //   file.type ===
+      //     'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ||
+      //   file.type === 'application/vnd.ms-excel'
+      // if (!isConform) {
+      //   message.error(`${file.name} 不符合上传文件的格式`)
+      //   return isConform || Upload.LIST_IGNORE
+      // }
+      let list = [...fileList, file]
+      setFileList(list)
+      copyFileList.current = list
+      return false // 返回false-手动上传文件
     },
+
     fileList,
   }
 
+  // 上传
   const handleOk = async () => {
-    message.loading('')
+    const params = await form.validateFields()
+    if (fileList.length == 0 && !Boolean(params.mobile)) {
+      message.warning('请输入手机号码或上传文件')
+      return
+    }
+    // 上传输入的手机号
+    message.loading({
+      content: '',
+      duration: 0,
+    })
+    if (Boolean(params.mobile)) {
+      uploadBlackMobileList({
+        ...params,
+        list_id: blackStore.activeBlack?.id,
+      })
+    }
+    if (fileList.length > 0) {
+      await uploadFileEvent(0)
+    }
+  }
+
+  // 上传文件
+  const uploadFileEvent = async (index: number) => {
+    const uid = fileList[index].uid
     try {
-      const params = await form.validateFields()
-      let promiseList = [
-        uploadBlackMobileList({
-          ...params,
-          list_id: blackStore.activeBlack?.id,
-        }),
-      ]
-      fileList.forEach((item) =>
-        promiseList.push(
-          uploadBlackMobileList({
-            file: item,
-            list_id: blackStore.activeBlack?.id,
-            mobile: '',
-          }),
-        ),
+      await uploadBlackMobileList({
+        file: fileList[index],
+        list_id: blackStore.activeBlack?.id,
+        mobile: '',
+      })
+      copyFileList.current = copyFileList.current.filter(
+        (item) => item.uid != uid,
       )
-      await Promise.all(promiseList)
-      message.destroy()
-      message.success('保存成功！')
-      setIsModalOpen(false)
-      onSearch()
     } catch (error) {
-      message.destroy()
-      message.error('存在上传失败的文件')
+      // message.error('存在上传失败的文件')
+    }
+
+    if (index == fileList.length - 1) {
+      // 全部上传成功
+      if (copyFileList.current.length == 0) {
+        message.destroy()
+        message.success('保存成功！')
+        setIsModalOpen(false)
+      } else {
+        // 存在上传失败的文件
+        message.error('存在上传失败的文件，请检查文件')
+      }
+      setFileList(copyFileList.current)
+      onSearch()
+    } else {
+      await uploadFileEvent(index + 1) // 递归上传
     }
   }
 
@@ -143,6 +164,8 @@ const Dialog = ({ onSearch }: Props, ref: any) => {
                 className='color-gray'
                 style={{ fontSize: '12px', margin: '0px' }}>
                 仅支持 TXT , CSV, VCF , excel 格式
+                <br />
+                超过十万条手机号建议使用CSV或TXT格式
               </p>
             </Upload>
           </div>
--- src/pages/console/blackList/right/index.tsx
+++ src/pages/console/blackList/right/index.tsx
@@ -104,7 +104,7 @@ export default function Right() {
         setCurrent(_lastCurrent)
       }
     } catch (error) {
-      console.log(error)
+      // console.log(error)
     }
   }
   // 点击分页
