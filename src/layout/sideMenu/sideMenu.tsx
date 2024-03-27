5d2ce1036a45715225972f4e2a2d7ef92b2909bb
zhutianwen <45391837+zhutianwen@users.noreply.github.com>
2024-03-27 16:31:07 +0800

    失败任务处理列表添加未处理 提示

--- src/layout/sideMenu/sideMenu.tsx
+++ src/layout/sideMenu/sideMenu.tsx
@@ -2,14 +2,17 @@ import { useState, useEffect } from 'react'
 import { NavLink, RouteObject, useMatches, useLocation } from 'react-router-dom'
 import { routerList } from '@/routes'
 import { useAppDispatch, useAppSelector } from '@/store/hook'
+import { getHandlerList } from '@/api'
+import { API } from 'apis'
 import './sideMenu.scss'
 import { useSize } from '@/hooks'
 import { closeIt, openIt, menuCloseStatus } from '@/store/reducers/menu'
-
+interface DataType extends API.GetHandlerListItem {}
 // 二级导航
 export default function SideMenu() {
   const location = useLocation()
   const matchList = useMatches()
+  const [faliTaskCount, setFaliTaskCount] = useState(null)
   const currentMatchBaseRouteObj = matchList.find(
     (match) => !match.id.includes('-'),
   )
@@ -36,6 +39,33 @@ export default function SideMenu() {
   // menu状态
   const close = useAppSelector(menuCloseStatus)
 
+  useEffect(() => {
+    let intervalId = null
+    const getFailTask = async () => {
+      try {
+        const params = {
+          mail: '',
+          page: '',
+          limit: '50',
+        }
+        const res = await getHandlerList(params)
+
+        let list: DataType[] = res.data.data.map((item, index) => {
+          let obj = { ...item, index: `${index}` }
+          return obj
+        })
+        let counts = list.filter((list) => list.flg == '1').length
+        setFaliTaskCount(counts)
+      } catch (error) {}
+    }
+    getFailTask()
+    intervalId = setInterval(getFailTask, 5000) // 每5秒调用一次
+    return () => {
+      clearInterval(intervalId) // 清除定时器
+    }
+  }, [])
+  // 获取失败任务处理列表
+
   return (
     <div
       data-class='sidemenu'
@@ -47,7 +77,15 @@ export default function SideMenu() {
             menu.children.map((ditem) => (
               <dd className='fn14' key={ditem.path}>
                 <NavLink to={`${menu.path}/${ditem.path}`}>
-                  {ditem.handle.alias}
+                  {/* {ditem.handle.alias} */}
+                  {menu.path == 'customer' && ditem.path == 'failTask' ? (
+                    <>
+                      {ditem.handle.alias}
+                      <span className='fail-badge'>{faliTaskCount}</span>
+                    </>
+                  ) : (
+                    <>{ditem.handle.alias}</>
+                  )}
                 </NavLink>
               </dd>
             ))}
